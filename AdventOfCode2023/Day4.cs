72ccc69d93c11645a406993a40593aff309e6d4e
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2024-02-15 19:01:51 +0000

    Day4

---
+++ AdventOfCode2023/Day4.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode2023;
+
+public class Day4 : IDay
+{
+    public string RunPartOne(string[] inputLines)
+    {
+        return inputLines.Select(Parse).Select(c => 2 ^ (c.MatchCount())).Sum().ToString();
+    }
+
+    public string RunPartTwo(string[] inputLines)
+    {
+        return "";
+    }
+
+    public Card Parse(string line)
+    {
+        var bits = line.Split(':')[1].Split('|');
+        var winning = bits[0].Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).Select(s => int.Parse(s.Trim())).ToArray();
+        var numbers = bits[1].Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).Select(s => int.Parse(s.Trim())).ToArray();
+
+        return new Card(winning, numbers);
+    }
+
+    public record Card(int[] WinningNumbers, int[] CardNumbers)
+    {
+        public int MatchCount() => CardNumbers.Intersect(WinningNumbers).Count();
+    }
+}
